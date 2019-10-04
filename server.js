97b5409bc45927a76e1bd7cc483c6e5b13c6bf98
mnjuhn <mnjuhn@gmail.com>
2019-10-03 20:13:18 -0400

    Adding an Node.js and MongoDB example for doing a geospatial query based on all points residing in one search radius.

---
+++ server.js
@@ -0,0 +1,53 @@
+iapp.post("/api/findEvents", function(req, res){
+
+  // get user id from session
+  searchRadius = req.body.radius;
+  var session = { '_id': req.body.sessionID }
+  sessModel.findOne(session)
+    .exec(function (err, sess) {
+      var errorObj = {};
+      if (err) {
+        errorObj['Error'] = err;
+        res.send(errorObj);
+      } else if (!sess) {
+        errorObj['Error'] = "No such session exists!";
+        res.send(errorObj);
+      }
+
+      // otherwise add event on sessions user id
+      else {
+        // get user's geo location and compare it to events geo location
+        var u_id = {'_id': sess['userID']};
+        usersModel.findOne(u_id)
+          .exec(function(err, user) {
+          var errorObj = {};
+          if (err) {
+            errorObj['Error'] = err;
+            res.send(errorObj);
+          } else if (!user) {
+            errorObj['Error'] = "No such user exists! Please re-login!";
+            res.send(errorObj);
+          }
+          // get user's geocode location and do a geocode search on radius km from location of puser
+          else {
+            var radians = searchRadius / 6371;
+            eventsModel.find({ location: { $geoWithin: { $centerSphere: [ [user.location.coordinates[0],
+              user.location.coordinates[1]], radians ]}}}).exec(function(err, events) {
+              var errorObj = {};
+              if (err) {
+                console.log(err);
+                errorObj['Error'] = err;
+                res.send(errorObj);
+              } else {
+                console.log(events);
+                res.send(events);
+              }
+            });
+          }
+
+        });
+      }
+
+    });
+
+});
